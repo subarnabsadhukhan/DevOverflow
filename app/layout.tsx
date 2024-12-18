fbed233427e936d3cd338b762e7fb429e3335333
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2024-12-18 18:47:58 +0530

    fix eslint errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+// eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
