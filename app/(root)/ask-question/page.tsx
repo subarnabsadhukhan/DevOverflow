7282f4d2bd7a35f701d5e222d3aaec9498f42dd6
Subarnab Sadhukhan <git@subarnabsadhukhan.com>
2024-12-18 18:39:53 +0530

    test webhook

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import Question from "@/components/forms/Question";
+import { auth } from "@clerk/nextjs/server";
+import { getUserById } from "@/lib/actions/user.action";
+import { redirect } from "next/navigation";
 
-const AskQuestion = () => {
-  return <div>AskQuestion</div>;
+const Page = async () => {
+  const { userId } = await auth();
+
+  if (!userId) redirect("/sign-in");
+
+  const mongoUser = await getUserById({ userId });
+
+  console.log(mongoUser);
+
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+
+      <div className="mt-9">
+        <Question mongoUserId={JSON.stringify(mongoUser._id)} />
+      </div>
+    </div>
+  );
 };
 
-export default AskQuestion;
+export default Page;
---
+++ app/api/webhooks/route.ts
@@ -0,0 +1,105 @@
+/* eslint-disable camelcase */
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const SIGNING_SECRET = process.env.SIGNING_SECRET;
+
+  if (!SIGNING_SECRET) {
+    throw new Error(
+      "Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local",
+    );
+  }
+
+  // Create new Svix instance with secret
+  const wh = new Webhook(SIGNING_SECRET);
+
+  // Get headers
+  const headerPayload = await headers();
+  const svixId = headerPayload.get("svix-id");
+  const svixTimestamp = headerPayload.get("svix-timestamp");
+  const svixSignature = headerPayload.get("svix-signature");
+
+  // If there are no headers, error out
+  if (!svixId || !svixTimestamp || !svixSignature) {
+    return new Response("Error: Missing Svix headers", {
+      status: 400,
+    });
+  }
+
+  // Get body
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+
+  let evt: WebhookEvent;
+
+  // Verify payload with headers
+  try {
+    evt = wh.verify(body, {
+      "svix-id": svixId,
+      "svix-timestamp": svixTimestamp,
+      "svix-signature": svixSignature,
+    }) as WebhookEvent;
+  } catch (err) {
+    console.error("Error: Could not verify webhook:", err);
+    return new Response("Error: Verification error", {
+      status: 400,
+    });
+  }
+
+  if (evt.type === "user.created") {
+    const { id, email_addresses, username, first_name, last_name, image_url } =
+      evt.data;
+
+    const mongoUser = await createUser({
+      clerkId: id,
+      name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
+      username: username || "",
+      picture: image_url,
+      email: email_addresses[0].email_address,
+    });
+
+    return NextResponse.json({
+      message: "OK",
+      user: mongoUser,
+    });
+  }
+
+  if (evt.type === "user.updated") {
+    const { id, email_addresses, username, first_name, last_name, image_url } =
+      evt.data;
+
+    const mongoUser = await updateUser({
+      clerkId: id,
+      updateData: {
+        name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
+        username: username || "",
+        picture: image_url,
+        email: email_addresses[0].email_address,
+      },
+      path: `/profile/${id}`,
+    });
+
+    return NextResponse.json({
+      message: "OK",
+      user: mongoUser,
+    });
+  }
+  if (evt.type === "user.deleted") {
+    const { id } = evt.data;
+
+    const deletedUser = await deleteUser({
+      clerkId: id!,
+    });
+
+    return NextResponse.json({
+      message: "OK",
+      user: deletedUser,
+    });
+  }
+
+  return new Response("Webhook received", { status: 201 });
+}
